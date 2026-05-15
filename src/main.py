52baa92d38c02d7aa0379b928f150fa98b749abf
Talal Atef <talal.atef.33@gmail.com>
2026-05-15 03:50:59 +0300

    parameterized path

--- src/main.py
+++ src/main.py
@@ -5,3 +5,20 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"data": {"message": "Hello World!"}}
+
+
+# parameterized path
+@app.get("/blogs/{blog_id}")
+def read_blog(blog_id: int):
+    return {"data": {"blog_id": blog_id}}
+
+
+@app.get("/blogs/{blog_id}/comments")
+def read_blog_comments(blog_id: int):
+    return {"data": {"blog_id": blog_id, "comments": ["comment1", "comment2"]}}
+
+# bug when we have two paths with same prefix, 
+# the first one will be executed and the second one will never be executed
+# @app.get("/blogs/unpublished")
+# def read_unpublished_blogs():
+#     return {"data": {"blogs": ["blog1", "blog2"]}}
